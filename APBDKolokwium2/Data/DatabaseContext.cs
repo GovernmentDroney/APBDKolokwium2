4685e8c51aec10cb02d56b2215a70daf54b1627d
Zofia <s31674@pjwstk.edu.pl>
2025-06-13 01:14:16 +0200

    1 am commit

---
+++ APBDKolokwium2/Data/DatabaseContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace APBDKolokwium2.Data;
+
+public class DatabaseContext : DbContext
+{
+    //public DbSet...
+    
+    
+    protected DatabaseContext()
+    {
+        
+    }
+
+    public DatabaseContext(DbContextOptions options) : base(options)
+    {
+        
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        
+    }
+}
---
+++ APBDKolokwium2/Services/Service.cs
@@ -0,0 +1,13 @@
+﻿using APBDKolokwium2.Data;
+
+namespace APBDKolokwium2.Services;
+
+public class Service : IService
+{
+    private readonly DatabaseContext _context;
+
+    public Service(DatabaseContext context)
+    {
+        _context = context;
+    }
+}
